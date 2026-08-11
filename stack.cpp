f788044cc3164c624f9d5501ccd55c67f428224d
Manikarnika Bajpai <manikarnikabajpai15@gmail.com>
2026-08-11 14:00:43 +0530

    stock span problem

--- stack.cpp
+++ stack.cpp
@@ -92,7 +92,7 @@ int main(){
     cout<<s.top();
     return 0;
 };  */        
-#include<iostream> 
+/*#include<iostream> 
 #include<stack>
 #include<string>       
 using namespace std;
@@ -138,11 +138,11 @@ int main()   {
     s.push(1);
    /*push_at_btm(s,4);
     while(!s.empty()){
-        cout<<s.top()<<" ";
+        cout<<s.top()<<" ";/
         s.pop();
     }*/
    /*stack<char>s;
-   rev_string(s,"abcd");*/
+   rev_string(s,"abcd");
    rev_stack(s);
    
     return 0;
@@ -150,4 +150,36 @@ int main()   {
 
     
     
+}*/
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+void stock_span(vector<int> stock, vector<int> span){
+    stack<int> s;
+    s.push(0);
+    span[0]=1;
+    for(int i=1; i<stock.size(); i++){
+        int currPrice= stock[i];
+        while(!s.empty() && currPrice>=stock[s.top()]){
+            s.pop();
+        };
+        if(s.empty()){
+            span[i]= i+1;
+        }
+        else{
+            int prevHigh= s.top();
+            span[i]= i-prevHigh;
+        };
+    };
+    for(int i=0; i<span.size(); i++){
+        cout<<span[i]<<" ";
+    }
+};
+int main(){
+    vector<int> stock = {100,80,60,70,60,85,100};
+    vector<int> span = {0,0,0,0,0,0,0};
+    stock_span(stock, span);
+    
+    return 0;
 }
