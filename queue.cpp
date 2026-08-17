69f623127efd157b77a52410b55fab489bd84804
Manikarnika Bajpai <manikarnikabajpai15@gmail.com>
2026-08-17 23:21:02 +0530

    queue reversal

--- queue.cpp
+++ queue.cpp
@@ -229,7 +229,7 @@ class queue{
     return 0;
   }*/
  //interleave 2 queues
- #include<iostream>
+ /*#include<iostream>
  #include<queue>
  using namespace std;
 
@@ -255,6 +255,34 @@ class queue{
     };
 
     return 0;
- }
+ }*/
+//queue reversal
+/*#include<iostream>
+#include<stack>
+#include<queue>
+using namespace std;
+int main(){
+    stack<int> s;
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    while(!q.empty()){
+        s.push(q.front());
+        q.pop();
+    };
+    while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+    };
+    while(!q.empty()){
+        cout<<q.front()<<" ";
+        q.pop();
+    };
+
+    return 0;
+}*/
 
    
