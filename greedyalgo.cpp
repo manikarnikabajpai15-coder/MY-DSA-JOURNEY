fc10dc970c83db75d70d9fc168788b1a8de8371e
Manikarnika Bajpai <manikarnikabajpai15@gmail.com>
2026-08-29 17:24:06 +0530

    adding files

--- greedyalgo.cpp
+++ greedyalgo.cpp
@@ -3,8 +3,8 @@
 //start a loop from 0 to n and select all nonoverlapping activities
 //overlapping condition-> 1st end>2nd start
 // nonoverlapping condition-> 1st end<=2nd start
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
+
 using namespace std;
 int maxActivities(vector<int> start, vector<int> end){
     int count=1;
@@ -17,11 +17,38 @@ int maxActivities(vector<int> start, vector<int> end){
         
     };
 return count;
+};
+bool compare(pair<int,int> p1, pair<int,int>p2){
+    return p1.second<p2.second;
 }
 int main(){
-    vector<int> start={1,3,0,5,8,5};
+    /*vector<int> start={1,3,0,5,8,5};
     vector<int> end={2,4,6,7,9,9};
-    cout<<maxActivities(start,end);
+    cout<<maxActivities(start,end);*/
+    vector<int> start={0,1,2};
+    vector<int> end={9,2,4};
+    vector<pair<int,int>> activity(3,make_pair(0,0));
+    activity[0]= make_pair(0,9);
+    activity[1]= make_pair(1,2);
+    activity[2]= make_pair(2,4);
+    for(int i=0; i<activity.size(); i++){
+        cout<<i<<":"<<activity[i].first<<","<<activity[i].second<<endl;
+    };
+    cout<<"after sorting------";
+    sort(activity.begin(),activity.end(),compare);
+    for(int i=0; i<activity.size(); i++){
+        cout<<i<<":"<<activity[i].first<<","<<activity[i].second<<endl;
+        
+
+    };
+
+
+    
+
+
+
+
+
 
     return 0;
 }
